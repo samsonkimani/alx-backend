5fd858aae749a00e58279b3a8ba3db111bdc5b9d
samsonkimani <samsonkimani43@gmail.com>
2023-07-26 23:51:09 +0300

    creating the lifo caching algorithm

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -13,20 +13,22 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ initializing the init method"""
         super().__init__()
+        self.key_indexes = []
 
     def put(self, key, item):
         """ method to add elements into the cache_data"""
-        if key is None or item is None:
-            return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            if key not in self.cache_data:
-                last_key = next(reversed(self.cache_data))
-                self.cache_data.pop(last_key)
-                print("DISCARD: {}".format(last_key))
-            else:
-                self.cache_data[key] = item
-        self.cache_data[key] = item
-        return self.cache_data
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.key_indexes.remove(key)
+                else:
+                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
+                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    print("DISCARD:", item_discarded)
+
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
 
     def get(self, key):
         """ return the dictionary item"""
