20fbb3c868665c7e6b8453be5c39429e9fc7f6eb
samsonkimani <samsonkimani43@gmail.com>
2023-07-27 00:01:55 +0300

    creating the least recently used caching algorithm

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""LRU Caching
+"""
+import collections
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU caching algorithm
+    """
+    def __init__(self):
+        """Constructor
+        """
+        super().__init__()
+        self.cache_data = collections.OrderedDict()
+        self.lru = None
+
+    def put(self, key, item):
+        """Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+            self.lru = key
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard = self.cache_data.popitem(last=False)
+            print('DISCARD: {}'.format(discard[0]))
+
+    def get(self, key):
+        """
+        return elements based on the keys
+        """
+        if key in self.cache_data:
+            self.lru = key
+            return self.cache_data[key]
+        return None
