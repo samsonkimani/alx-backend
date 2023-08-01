5237465b708df03022cbd21b0de76dfae3db2f21
samsonkimani <samsonkimani43@gmail.com>
2023-08-01 15:26:48 +0300

    creating a simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+creating a flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """ an endpoint to the index page"""
+    return render_template('index.html')
