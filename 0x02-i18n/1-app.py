87e3cad879d7a8308ab616d3d6dbec3a6f6dc3a0
samsonkimani <samsonkimani43@gmail.com>
2023-08-01 15:47:26 +0300

    configuring babel in flask app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+creating a flask app
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ config class to set babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def home():
+    """ an endpoint to the index page"""
+    return render_template('index.html')
