dfe5343e5ef1bcfab019d58fc9ebb61c2de79ea3
samsonkimani <samsonkimani43@gmail.com>
2023-08-01 22:43:25 +0300

    corrections

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -10,6 +10,13 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
 
 class Config:
     """ config class to set babel"""
@@ -20,12 +27,6 @@ class Config:
 
 app.config.from_object(Config)
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
 
 @babel.localeselector
 def get_locale():
@@ -36,18 +37,21 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
+
 def get_user():
     get_id = request.args.get("login_as")
 
-    if get_id and get_id in users:
-        return users.get(int(login_id))
+    if get_id:
+        return users[int(get_id)]
     else:
         return None
+
 
 @app.before_request
 def before_request():
     user = get_user()
     g.user = user
+
 
 @app.route("/")
 def home():
