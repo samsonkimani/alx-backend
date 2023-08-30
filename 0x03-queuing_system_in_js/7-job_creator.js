2e2b2f9416234c96b3a4d53e24198973ff87c75e
samsonkimani <samsonkimani43@gmail.com>
2023-08-30 17:56:07 +0300

    updating the jobs progress

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -66,4 +66,8 @@ jobs.forEach((jobData) => {
   job.on('failed', (error) => {
     console.log(`Notification job ${job.id} failed ${error}`);
   });
+
+  job.on('progress', (progress, data) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
 });
